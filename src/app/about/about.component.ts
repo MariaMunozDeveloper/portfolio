0ce860f4474951fc9a42803048ae0d894484eae4
Mireia <munozferrer.maria@gmail.com>
2026-06-08 17:19:17 +0200

    feat: cursos y carta de recomendación

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { RevealDirective } from '../shared/reveal.directive';
 
 interface StackItem {
   name: string;
   type: string;
+}
+
+interface Course {
+  name: string;
+  date: string;
+  skill: string;
 }
 
 @Component({
@@ -14,6 +21,24 @@ interface StackItem {
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  showCourses = false;
+  showLetter = false;
+
+  letterUrl: SafeResourceUrl;
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.letterUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+        'https://drive.google.com/file/d/1EpcpN0OJ8TBcNRpTzQH4VbSFMow1L3N8/preview'
+    );
+  }
+
+  courses: Course[] = [
+    { name: 'Ultimate GIT: gestiona tus equipos y trabaja en equipo', date: 'Jun. 2026', skill: 'Git' },
+    { name: 'Angular: De cero a experto', date: 'Jun. 2026', skill: 'Angular' },
+    { name: 'Aprende MongoDB desde cero', date: 'Mar. 2026', skill: 'MongoDB' },
+    { name: 'Spring Framework & Spring Boot', date: 'Mar. 2026', skill: 'Spring Boot' },
+  ];
+
   stack: StackItem[] = [
     { name: 'Angular', type: 'Frontend' },
     { name: 'TypeScript', type: 'Language' },
