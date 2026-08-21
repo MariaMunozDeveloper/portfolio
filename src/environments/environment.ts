b99e9497b41510f7210192179e298dc164d519d5
Mireia <munozferrer.maria@gmail.com>
2026-08-21 15:13:56 +0200

    feat: add Firebase Firestore setup and environment variables

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyDK8L-A5wce4jG7-2yHdIICmiXcD4YdjKM",
+        authDomain: "mireia-portfolio.firebaseapp.com",
+        projectId: "mireia-portfolio",
+        storageBucket: "mireia-portfolio.firebasestorage.app",
+        messagingSenderId: "698287413156",
+        appId: "1:698287413156:web:c1fac5528d12cb5e6c9331"
+    }
+};
