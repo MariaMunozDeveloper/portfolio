2dccacca390c75f6b304c31d5830fd34b31a6de3
Mireia <munozferrer.maria@gmail.com>
2026-08-21 15:22:23 +0200

    feat: add visit counter to home component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {VisitCounterService} from "../visit-counter.service";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  visits = 0;
+
+  constructor(private visitCounter: VisitCounterService) {}
+
+  ngOnInit() {
+    this.visitCounter.getAndIncrementVisits().subscribe(count => {
+      this.visits = count;
+    });
+  }
+}
